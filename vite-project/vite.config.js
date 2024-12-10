5c6f54a7c1073dd9c2b48950be0338f553d52617
Cupido0nn <malaucierno@mail.com>
2024-12-10 18:09:04 -0300

    djaw9oiud

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,10 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Especificar el directorio de salida
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        // Asegúrate de que los archivos JavaScript tengan el tipo MIME correcto
+        format: 'esm',
+        entryFileNames: '[name]-[hash].js',
+        chunkFileNames: '[name]-[hash].js',
+        assetFileNames: '[name]-[hash][extname]',
+        globals: {
+          'react/jsx-dev-runtime': 'ReactJSXDevRuntime',
+          'react-dom/jsx-dev-runtime': 'ReactDOMJSXDevRuntime'
+        }
+      }
+    }
   }
 })
+
